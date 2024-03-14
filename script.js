cf437a93ae099c051f8abe04b12a2a4cd56f1ac6
Narwhal-hehe <161725910+Narwhal-hehe@users.noreply.github.com>
2024-03-14 21:10:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function sayHello(){
+  var response = prompt("What is your name?");
+  alert('Hello '+ response + ' ，have a nice day!');
+}
+//sayHello();
+function showTopic(){
+  var x = document.getElementById('demo');
+  x.style.fontSize = '25px';
+  x.style.color = 'red'
+}
